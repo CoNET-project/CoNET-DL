7a86f092236b70f7a8ccbece801a0efe4ba2f29b
CoNET Project <info@CoNETTech.ca>
2024-05-08 03:23:26 -0700

    add GAS based margin

--- src/util/daemon.ts
+++ src/util/daemon.ts
@@ -257,6 +257,8 @@ const CalculateReferrals = async (walletAddress: string, totalToken: string, rew
 	return CallBack(null, {addressList, payList})
 }
 
+
+
 const transferCCNTP = (provateKey: string, walletList: string[], PayList: string[], callback: () => void) => {
 	if (walletList.length < 1) {
 		return callback()
@@ -305,5 +307,86 @@ const startDaemonProcess = async (block: number) => {
 	guardianReferrals()
 }
 
+interface transferObj {
+	privateKey: string
+	walletList: string[]
+	payList: string []
+}
 
-startListeningCONET_Holesky_EPOCH()
+const transferPool: transferObj[] = []
+let startTransfering = false
+let marginPooling = false
+
+const marginPool = () => {
+	if (marginPooling) {
+		return logger(Color.red(`marginPool stoped because other marginPool is processing!`))
+	}
+	marginPooling = true
+	const reTransferPool: transferObj[] = []
+
+	const next = (obj: transferObj) => {
+		const index = transferPool.findIndex(n => n.privateKey === obj.privateKey)
+		if (index < 0) {
+			const margened = mergeTransfers(obj.walletList, obj.payList)
+			obj.walletList = margened.walletList
+			obj.payList = margened.payList
+			return
+		}
+		const _obj = transferPool.splice(index, 1)
+		_obj[0].walletList.forEach((n, nI) => {
+			obj.walletList.push(n)
+			obj.payList.push(_obj[0].payList[nI])
+		})
+		next (obj)
+	}
+
+	const margin = () => {
+
+		const obj = transferPool.shift()
+		if (!obj) {
+			reTransferPool.forEach(n => {
+				transferPool.push (n)
+			})
+			marginPooling = false
+			startTransfer ()
+			return 
+		}
+		next(obj)
+		reTransferPool.push(obj)
+		margin()
+	}
+	margin()
+}
+
+const startTransfer = async () => {
+	if (startTransfering) {
+		return
+	}
+	startTransfering = true
+
+	const provideCONET = new ethers.JsonRpcProvider(conet_Holesky_rpc)
+	const feeData = await provideCONET.getFeeData()
+	const gasPrice = feeData.gasPrice ? parseFloat((feeData.gasPrice/BigInt(10**9)).toString()) : 0
+
+	if (gasPrice > 5 || !gasPrice) {
+		startTransfering = false
+		marginPool()
+		return logger(Color.red(`startTransfer GAS [${gasPrice}] > 5 || gasPrice === 0, waiting to Low! transferPool legnth = [${transferPool.length}]`))
+	}
+	
+	const obj = transferPool.shift()
+
+	if (!obj) {
+		startTransfering = false
+		return logger(Color.grey(`startTransfer Pool Empty, STOP startTransfer  GAS fee is [${gasPrice}]`))
+	}
+
+	return transferCCNTP(obj.privateKey, obj.walletList, obj.payList, () => {
+		startTransfering = false
+		startTransfer ()
+	})
+}
+
+startTransfer()
+
+// startListeningCONET_Holesky_EPOCH()
