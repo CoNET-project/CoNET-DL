1669a5e77047243ab5de11c974bdec86dbe130c6
CoNET Project <info@CoNETTech.ca>
2024-05-19 07:00:03 -0700

    test

--- src/util/CalculateReferrals.ts
+++ src/util/CalculateReferrals.ts
@@ -1,6 +1,10 @@
 import {ConnectionConfig, createConnection} from 'mysql'
 import { logger } from './logger'
 import {inspect} from 'node:util'
+import {ethers} from 'ethers'
+import {conet_Referral_contractV2} from './util'
+import {abi as CONET_Referral_ABI} from './conet-referral.json'
+
 
 const connectObj:ConnectionConfig = {
 	host     : 'localhost',
@@ -13,40 +17,63 @@ const mySql = createConnection (connectObj)
 const getReferrer = (address: string, callbak: (err: Error|null, data?: any) => void)=> {
 	const _address = address.toLowerCase()
 	const query = `SELECT wallet FROM referrer WHERE id = '${_address}'`
-	mySql.query(query, (err, results, fields) => {
+	return mySql.query(query, (err, results, fields) => {
 		if (err) {
 			return callbak (err)
 		}
 		logger(`getReferrer results!`)
 		logger(inspect(results[0], false, 3, true))
-		callbak(null, results)
+		return callbak(null, results)
 	})
 }
 
-const saveReferrer = (id: string, address: string) => {
-	const _address = address.toLowerCase()
-	const _id = id.toLowerCase()
-	const query = `INSERT INTO referrer (id, wallet) VALUES ('${_id}', '${_address}')`
+const saveReferrer = (id: string, address: string) => new Promise( resolve => {
+	const query = `INSERT INTO referrer (id, wallet) VALUES ('${id}', '${address}')`
+	return mySql.query(query, (err, results, fields) => {
+		return resolve(true)
+	})
+})
 
-}
 
-const countReword = (reword: number, wallet: string, totalToken: number,  callback: (data: null|string)=> void) => {
-	return getReferrer(wallet, (err, data) => {
+const countReword = (reword: number, wallet: string, totalToken: number, callback: (data: null|{wallet: string,pay: string}) => void) => {
+	return getReferrer(wallet, async (err, data: any) => {
 		if (err) {
 			return callback (null)
 		}
-		// if (!data) {
-		// 	const wallet1 = await contract.getReferrer(wallet)
-		// }
-		// return callback((totalToken * reword).toString())
+		if (data) {
+			return ({wallet: data.RowDataPacket.wallet, pay: (totalToken * reword).toFixed(0)})
+		}
+		const conet_Holesky_rpc = 'https://rpc.conet.network'
+		const contract = new ethers.Contract(conet_Referral_contractV2, CONET_Referral_ABI, new ethers.JsonRpcProvider(conet_Holesky_rpc))
+		let address
+		try {
+			address = (await contract.getReferrer(wallet)).toLowerCase()
+		} catch (ex) {
+
+		}
+		if (address === '0x0000000000000000000000000000000000000000') {
+			return callback (null)
+		}
+		
+		await saveReferrer(wallet, address)
+		return ({wallet: address, pay: (totalToken * reword).toFixed(0)})
 	})
 }
 
-const CalculateReferrals = (walletAddress: string, totalToken: string, rewordArray: number[], CallBack: (err:Error|null, data?: any) => void) => {
+const constCalculateReferralsCallback = (addressList: string[], payList: string[], CallBack: (data?: any) => void) => {
+	return mySql.end(() => {
+		if (addressList.length <1) {
+			return CallBack (null)
+		}
+		return CallBack ({addressList, payList})
+	})
+}
+
+const CalculateReferrals = (walletAddress: string, totalToken: number, CallBack: (data?: any) => void) => {
 	return mySql.connect(err => {
 		if (err) {
 			return setTimeout(() => {
-				return CalculateReferrals (walletAddress, totalToken, rewordArray, CallBack)
+				return CalculateReferrals (walletAddress, totalToken, CallBack)
 			}, 500)
 			
 		}
@@ -55,37 +82,28 @@ const CalculateReferrals = (walletAddress: string, totalToken: string, rewordArr
 		const addressList: string[] = []
 		const payList: string[] = []
 
-		for (let i of [.05, .03, .01]) {
-			let address: string
-			// countReword(i, _walletAddress, data => {
-
-			// })
-			// try{
-			// 	address = ReferralsMap.get(_walletAddress) || await contract.getReferrer(_walletAddress)
-			// } catch (ex: any) {
-			// 	break
-			// }
-			
-			// // logger (colors.blue(`CalculateReferrals get address = [${address}]`))
-			// if (address === '0x0000000000000000000000000000000000000000') {
-			// 	break
-			// }
-
-			
-			// address = address.toLowerCase()
-			// ReferralsMap.set(_walletAddress, address)
-			// if (checkAddressArray.length) {
-			// 	const index = checkAddressArray.findIndex(n => n.toLowerCase() === address)
-			// 	if (index< 0) {
-			// 		return CallBack(new Error(`CalculateReferrals walletAddress [${_walletAddress}'s up layer address ${address}] hasn't in checkAddressArray! STOP CalculateReferrals`))
-			// 	}
-			// }
-			// addressList.push(address)
-			// payList.push((parseFloat(totalToken)*i).toString())
-			// _walletAddress = address
-		}
-
-		return CallBack(null, {addressList, payList})
+		return countReword(.05, _walletAddress, totalToken, data1 => {
+			if (!data1) {
+				return constCalculateReferralsCallback(addressList, payList, CallBack)
+			}
+			addressList.push(data1.wallet)
+			payList.push(data1.pay)
+			return countReword(.03, data1.wallet, totalToken, data2 => {
+				if (!data2) {
+					return constCalculateReferralsCallback(addressList, payList, CallBack)
+				}
+				addressList.push(data2.wallet)
+				payList.push(data2.pay)
+				return countReword(.01, data2.wallet, totalToken, data3 => {
+					if (!data3) {
+						return constCalculateReferralsCallback(addressList, payList, CallBack)
+					}
+					addressList.push(data3.wallet)
+					payList.push(data3.pay)
+					return constCalculateReferralsCallback(addressList, payList, CallBack)
+				})
+			})
+		})
 
 	})
 	
@@ -94,7 +112,7 @@ mySql.connect(err => {
 	if (err) {
 		return logger(err)
 	}
-	return getReferrer('0x8c96953df8ddf2ff9141be66d196c8bf69800e39', (err, data)=> {
+	return CalculateReferrals('0x8c96953df8ddf2ff9141be66d196c8bf69800e39', 16928327600000000, ( data)=> {
 		mySql.end(() => {
 			logger (`mySql.end!`)
 			logger(inspect(data, false, 3, true))
