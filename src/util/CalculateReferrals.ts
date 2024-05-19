4895fef66ac10591226afd7a4576932584397a04
CoNET Project <info@CoNETTech.ca>
2024-05-19 06:02:42 -0700

    test

--- src/util/CalculateReferrals.ts
+++ src/util/CalculateReferrals.ts
@@ -35,10 +35,10 @@ const countReword = (reword: number, wallet: string, totalToken: number,  callba
 		if (err) {
 			return callback (null)
 		}
-		if (!data) {
-			const wallet1 = await contract.getReferrer(wallet)
-		}
-		return callback((totalToken * reword).toString())
+		// if (!data) {
+		// 	const wallet1 = await contract.getReferrer(wallet)
+		// }
+		// return callback((totalToken * reword).toString())
 	})
 }
 
@@ -57,32 +57,32 @@ const CalculateReferrals = (walletAddress: string, totalToken: string, rewordArr
 
 		for (let i of [.05, .03, .01]) {
 			let address: string
-			countReword(i, _walletAddress, data => {
+			// countReword(i, _walletAddress, data => {
 
-			})
-			try{
-				address = ReferralsMap.get(_walletAddress) || await contract.getReferrer(_walletAddress)
-			} catch (ex: any) {
-				break
-			}
+			// })
+			// try{
+			// 	address = ReferralsMap.get(_walletAddress) || await contract.getReferrer(_walletAddress)
+			// } catch (ex: any) {
+			// 	break
+			// }
 			
-			// logger (colors.blue(`CalculateReferrals get address = [${address}]`))
-			if (address === '0x0000000000000000000000000000000000000000') {
-				break
-			}
+			// // logger (colors.blue(`CalculateReferrals get address = [${address}]`))
+			// if (address === '0x0000000000000000000000000000000000000000') {
+			// 	break
+			// }
 
 			
-			address = address.toLowerCase()
-			ReferralsMap.set(_walletAddress, address)
-			if (checkAddressArray.length) {
-				const index = checkAddressArray.findIndex(n => n.toLowerCase() === address)
-				if (index< 0) {
-					return CallBack(new Error(`CalculateReferrals walletAddress [${_walletAddress}'s up layer address ${address}] hasn't in checkAddressArray! STOP CalculateReferrals`))
-				}
-			}
-			addressList.push(address)
-			payList.push((parseFloat(totalToken)*i).toString())
-			_walletAddress = address
+			// address = address.toLowerCase()
+			// ReferralsMap.set(_walletAddress, address)
+			// if (checkAddressArray.length) {
+			// 	const index = checkAddressArray.findIndex(n => n.toLowerCase() === address)
+			// 	if (index< 0) {
+			// 		return CallBack(new Error(`CalculateReferrals walletAddress [${_walletAddress}'s up layer address ${address}] hasn't in checkAddressArray! STOP CalculateReferrals`))
+			// 	}
+			// }
+			// addressList.push(address)
+			// payList.push((parseFloat(totalToken)*i).toString())
+			// _walletAddress = address
 		}
 
 		return CallBack(null, {addressList, payList})
