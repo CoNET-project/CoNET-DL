5658e9ab18373ec740d15694910c9af57d1385aa
CoNET Project <info@CoNETTech.ca>
2024-05-20 11:16:40 -0700

    test

--- src/util/ReferralsDaemonV3.ts
+++ src/util/ReferralsDaemonV3.ts
@@ -7,46 +7,41 @@ import {exec} from 'node:child_process'
 const conet_Holesky_rpc = 'https://rpc.conet.network'
 
 
-let EPOCH = 0
-let transferEposh = 0
+const startListeningCONET_Holesky_EPOCH = async () => {
+	const provideCONET = new ethers.JsonRpcProvider(conet_Holesky_rpc)
 
-
-
-
-
-	const startListeningCONET_Holesky_EPOCH = async () => {
-		const provideCONET = new ethers.JsonRpcProvider(conet_Holesky_rpc)
-		EPOCH = await provideCONET.getBlockNumber()
-		transferEposh = EPOCH - 3
-
-		logger(Color.magenta(`startListeningCONET_Holesky_EPOCH [${EPOCH}] start!`))
-		provideCONET.on('block', async block => {
-			if (block <= EPOCH) {
-				return logger(Color.red(`startListeningCONET_Holesky_EPOCH got Event ${block} < EPOCH ${EPOCH} Error! STOP!`))
-			}
-			return startDaemonProcess(parseInt(block.toString()))
-		})
-	}
-
-	const startDaemonProcess = async (block: number) => {
-		console.log('')
-		EPOCH = block -3
-		doWorker(EPOCH.toString())
-	}
-
-	const doWorker = (epoch: string) => new Promise(resolve => {
-		const command = `node dist/util/CalculateReferrals epoch=${epoch}`
-		return exec(command, (error, stdout, stderr) => {
-			const ret = stdout.split('ret=')[1]
-			try{
-				const ret1 = JSON.parse(ret)
-				return resolve (ret1)
-			} catch (ex) {
-				logger(Color.red(`doWorker JSON.parse(ret) Error! ret=${ret}`))
-			}
-			return resolve (null)
-		})
+	provideCONET.on('block', async block => {
+		return startDaemonProcess(parseInt(block.toString()))
 	})
+}
 
-	startListeningCONET_Holesky_EPOCH()
+const startDaemonProcess = async (block: number) => {
+	console.log('')
+	doWorker((block -3).toString())
+}
+
+const doWorkerCom: (command: string) => Promise<boolean> = (command: string) => new Promise(resolve => {
+	exec(command, (error, stdout, stderr) => {
+		const ret = stdout.split('ret=')[1]
+		try{
+			const ret1 = JSON.parse(ret)
+			return resolve (true)
+		} catch (ex) {
+			logger(Color.red(`doEpochNode JSON.parse(ret) Error! ret=${ret}`))
+		}
+		return resolve (false)
+	})
+})
+
+const doWorker = async (epoch: string) => {
+	const command = `node dist/util/doEpochNode epoch=${epoch}`
+	const command1 = `node dist/util/doEpoch epoch=${epoch}`
+	await Promise.all ([
+		doWorkerCom(command),
+		doWorkerCom(command1)
+	])
+	logger(`doWorker Epoch ${epoch} Finished`)
+}
+
+startListeningCONET_Holesky_EPOCH()
 
