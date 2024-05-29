84618aced11ba6f6234680c67091b44e131552ea
CoNET Project <info@CoNETTech.ca>
2024-05-29 13:50:43 -0700

    test

--- src/endpoint/checkIpAdderss.ts
+++ src/endpoint/checkIpAdderss.ts
@@ -7,8 +7,8 @@ const startFilter = () => {
 			return logger(`startFilter error!`)
 		}
 		const kk = data.toString()
-		const ll = kk.split('\r\n')
-		logger(JSON.stringify(kk))
+		const ll = kk.split('\n')
+		
 		logger(Colors.red(`IP address Length [${ll.length}]`))
 		ll.forEach(n => {
 			const ipaddress = n.split(' ')[0]
