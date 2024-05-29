f7af03fd9ffa1cde2e3664f5570f5435b21b9284
CoNET Project <info@CoNETTech.ca>
2024-05-29 13:46:11 -0700

    test

--- src/endpoint/checkIpAdderss.ts
+++ src/endpoint/checkIpAdderss.ts
@@ -1,14 +1,18 @@
 import {readFile} from 'node:fs'
 import { logger } from '../util/logger'
-
+import Colors from 'colors/safe'
 const startFilter = () => {
 	return readFile('kk', (err, data) => {
 		if (err) {
 			return logger(`startFilter error!`)
 		}
 		const kk = data.toString()
-		kk.split('\r\n')
-		logger(kk)
+		const ll = kk.split('\r\n')
+		ll.forEach(n => {
+			const ipaddress = n.split(' ')[0]
+			logger(Colors.blue(`ipaddress [${ipaddress}]`))
+		})
+		
 	})
 }
 
