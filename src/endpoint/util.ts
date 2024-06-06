2e8c014708f283812b446fbb8f1fd4dc6f4f30e2
CoNET Project <info@CoNETTech.ca>
2024-06-06 12:40:42 -0700

    add node key

--- src/endpoint/util.ts
+++ src/endpoint/util.ts
@@ -3,6 +3,8 @@ import { join } from 'node:path'
 import { homedir, platform } from 'node:os'
 import cCNTPAbi from '../util/cCNTP.json'
 import { logger } from '../util/util'
+
+import Colors from 'colors/safe'
 const setup = join( homedir(),'.master.json' )
 
 const CONET_Holesky_RPC = 'http://142.171.89.182:8000'
@@ -13,7 +15,7 @@ export const cntpAdminWallet = new ethers.Wallet(masterSetup.conetFaucetAdmin)
 
 
 const CNTP_Contract = new ethers.Contract(Claimable_CONET_Point_addr, cCNTPAbi)
-
+logger(Colors.blue(`Node Key = ${cntpAdminWallet.address}`))
 const getWalletsAsset = (wallets: string[]) => {
 	
 }
