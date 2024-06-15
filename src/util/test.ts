6816e77f21386a512a5ef0d4bce79bbbe5af5c79
CoNET Project <info@CoNETTech.ca>
2024-06-15 01:13:54 -0700

    test

--- src/util/test.ts
+++ src/util/test.ts
@@ -1,5 +1,5 @@
 import {sendClaimableAsset, masterSetup, realToClaimableContractAddress} from './util'
-import ethers from 'ethers'
+import {ethers} from 'ethers'
 const test = async () => {
 	const retClaimableContractAddress = realToClaimableContractAddress('wusdt')
 	await sendClaimableAsset(masterSetup.conetFaucetAdmin[0],  retClaimableContractAddress, '0xefdaA48BB1961AEfBF6d0295ea96FB034543995c', ethers.formatEther('2625'))
