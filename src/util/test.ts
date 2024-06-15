ce87cacb131b1e906bffd7b1716b44b78df2eb85
CoNET Project <info@CoNETTech.ca>
2024-06-15 01:24:43 -0700

    test

--- src/util/test.ts
+++ src/util/test.ts
@@ -1,8 +1,13 @@
-import {sendClaimableAsset, masterSetup, realToClaimableContractAddress} from './util'
+import {sendClaimableAsset, masterSetup, realToClaimableContractAddress, logger} from './util'
 import {ethers} from 'ethers'
+import Colors from 'colors/safe'
 const test = async () => {
+	const name = 'wusdt'
+	const wallet = '0xefdaA48BB1961AEfBF6d0295ea96FB034543995c'
 	const retClaimableContractAddress = realToClaimableContractAddress('wusdt')
-	await sendClaimableAsset(masterSetup.conetFaucetAdmin[0],  retClaimableContractAddress, '0xefdaA48BB1961AEfBF6d0295ea96FB034543995c', ethers.formatEther('2625'))
+	const units = ethers.parseUnits('2625', 'ether').toString()
+	logger(Colors.blue(` ${name} [${retClaimableContractAddress}] ${units} [${ethers.formatEther(units)}] to ${wallet}`))
+	await sendClaimableAsset(masterSetup.conetFaucetAdmin[0], retClaimableContractAddress, wallet,units)
 }
 
 test()
