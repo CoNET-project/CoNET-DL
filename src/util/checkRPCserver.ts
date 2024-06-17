f8b815192264cea4697ae018ac9521fd593cccd2
CoNET Project <info@CoNETTech.ca>
2024-06-17 08:20:45 -0700

    test

--- src/util/checkRPCserver.ts
+++ src/util/checkRPCserver.ts
@@ -1,14 +1,30 @@
 import {readFile} from 'node:fs'
 import {exec} from 'node:child_process'
 import { logger } from './logger'
+import {inspect} from 'node:util'
 import Colors from 'colors/safe'
 
 const serverFIle = `cat /etc/nginx/sites-available/conet.conf|grep "[^#]server\ [^{]"`
-const getAllServer = () => {
+const getAllServer = () => new Promise(resolve => {
+
 	exec(serverFIle, (err, stdout) => {
-		logger(Colors.blue(`getAllServer ${stdout}`))
+		if (!stdout) {
+			return resolve (false)
+		}
+		const servers: string[] = []
+		const k = stdout.split('\n')
+		k.forEach (n => {
+			const ll = n.split('server ')[1]
+			if (ll) {
+				servers.push (ll.split(';')[0])
+			}
+		})
+		return resolve (servers)
 	})
+})
+
+const test = async () => {
+	logger(inspect(await getAllServer(), false, 3, true))
 }
 
-
-getAllServer()
+test()
