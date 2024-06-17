7f053c65bdd623ffd0349a98cfde5e5ec09516aa
CoNET Project <info@CoNETTech.ca>
2024-06-17 08:15:53 -0700

    bug fix

--- src/util/checkRPCserver.ts
+++ src/util/checkRPCserver.ts
@@ -1,6 +1,6 @@
 import {readFile} from 'node:fs'
 import {exec} from 'node:child_process'
-import { logger } from './util'
+import { logger } from './logger'
 import Colors from 'colors/safe'
 
 const serverFIle = `cat /etc/nginx/sites-available/conet.conf|grep "[^#]server\ [^{]"`
