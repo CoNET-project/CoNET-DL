eb86670b2984ffabe2b85e12185d7dd8a2a85da3
CoNET Project <info@CoNETTech.ca>
2024-08-24 09:21:50 -0700

    test

--- src/util/CNTP_Transfer_pool.ts
+++ src/util/CNTP_Transfer_pool.ts
@@ -113,7 +113,6 @@ export default class CNTP_Transfer_Manager {
 		let groupCount = 0
 		let items = 0
 
-		logger(Color.magenta(`transferProcess pool size = ${this.pool.size} Max length = ${this.eachTransLength} split ${splitGroupNumber} Group wallets group size = ${wallets.map(n => n.length)}`))
 		let wallets: string[] = []
 		let pays: number [] = []
 		this.pool.forEach((v, key) => {
