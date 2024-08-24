29f3fc09c3bf4beb4ed1b539627254ed0c72c519
CoNET Project <info@CoNETTech.ca>
2024-08-24 09:04:06 -0700

    test

--- src/util/CNTP_Transfer_pool.ts
+++ src/util/CNTP_Transfer_pool.ts
@@ -139,7 +139,7 @@ export default class CNTP_Transfer_Manager {
 		let iii_1 = 0
 		logger(Color.magenta(`transferProcess pool size = ${this.pool.size} Max length = ${this.eachTransLength} split ${splitGroupNumber} Group wallets group size = ${wallets.map(n => n.length)}`))
 		await mapLimit(wallets, this.privatePayArray.length, async (n, next) => {
-			logger(Color.magenta(`start transferCNTP group [${iii_1}] wallets ${n.length} `))
+			logger(Color.magenta(`start transferCNTP group [${iii_1}] wallets ${n.length} pays length = ${pay[iii_1].length}`))
 			const waitTransfer = await this.transferCNTP(n, pay[iii_1], this.getPrivateWallet())
 			if (!waitTransfer) {
 				logger(Color.red(`transferCNTP got Error return transfer group wallet length [${ n.length }] pay length [${pay.length }]to Pool, current Pool size = ${this.pool.size}! `))
