d87ba2662691e45aae25b69f4b9a3881af517c86
CoNET Project <info@CoNETTech.ca>
2024-08-31 20:55:08 -0700

    game leadboard

--- src/endpoint/gameLeaderBoard.ts
+++ src/endpoint/gameLeaderBoard.ts
@@ -1,20 +1,20 @@
 import {ethers} from 'ethers'
 import {listAllLotte} from './help-database'
 import {writeFile} from 'node:fs'
-import {logger} from '../util/logger'
-import { inspect} from 'node:util'
+import {masterSetup} from '../util/util'
+
 const conetRPC = new ethers.JsonRpcProvider('https://rpc.conet.network')
 const startWriteReadboard = async (block:  number) => {
 	const list = await listAllLotte ()
 	await saveFragment('gaem_LeaderBoard', JSON.stringify(list))
 	console.log (`GameLeaderBoard [${block}] = gaem_LeaderBoard success`)
 }
+const storagePATH = masterSetup.storagePATH
 
-const storagePath = ['/home/peter/FragmentIPFS/FragmentIPFS1', '/home/peter/FragmentIPFS/FragmentIPFS2', '/home/peter/FragmentIPFS/FragmentIPFS3']
 const saveFragment = (hashName: string, data: string) => new Promise(resolve=> {
 	const lastChar = hashName[hashName.length-1]
 	const n = parseInt(`0x${lastChar}`, 16)
-	const path = storagePath[n%storagePath.length]
+	const path = storagePATH[n%storagePATH.length]
 	const fileName = `${path}/${hashName}`
 
 	return writeFile(fileName, data, err => {
