9b0c30fe1b9eb780f3c554d71248b51bec41f447
CoNET Project <info@CoNETTech.ca>
2024-09-26 16:30:03 -0700

    test

--- src/endpoint/serverApiV3Master.ts
+++ src/endpoint/serverApiV3Master.ts
@@ -65,7 +65,7 @@ const listeningGuardianNodes = async (block: number) => {
 	})
 }
 
-export const checkGasPrice = 400000000
+export const checkGasPrice = 250000000
 let startDailyPoolTranferProcess = false
 let lastTransferTimeStamp = new Date().getTime()
 const longestWaitingTime = 1000 * 60 * 10
