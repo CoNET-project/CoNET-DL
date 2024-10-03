9b07904bdc268fa1af9a08cec8d3ba4b62e42898
CoNET Project <info@CoNETTech.ca>
2024-10-03 03:50:15 -0700

    Create apiv4.ts

---
+++ src/endpoint/apiv4.ts
@@ -0,0 +1,38 @@
+import server from './serverApiV4'
+import Cluster from 'node:cluster'
+import { cpus } from 'node:os'
+import {logger} from '../util/logger'
+import Colors from 'colors/safe'
+import masterServer from './serverApiV4Master'
+
+if (Cluster.isPrimary) {
+
+	const forkWorker = () => {
+		
+		let numCPUs = cpus().length
+
+		for (let i = 0; i < numCPUs; i ++){
+			_forkWorker()
+		}
+	}
+	
+	const _forkWorker = () => {
+		const fork = Cluster.fork ()
+		fork.once ('exit', (code: number, signal: string) => {
+			logger (Colors.red(`Worker [${ fork.id }] Exit with code[${ code }] signal[${ signal }]!\n Restart after 30 seconds!`))
+			
+			return setTimeout (() => {
+				return _forkWorker ()
+			}, 1000 * 10 )
+		})
+		
+		return (fork)
+	}
+	setTimeout (() => {
+		forkWorker()
+	}, 5000)
+	
+	new masterServer()
+} else {
+	new server()
+}
