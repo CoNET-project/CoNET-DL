081837631e3f5a177c7fe657808cc2b186afab1c
CoNET Project <info@CoNETTech.ca>
2024-10-12 21:52:08 -0700

    Update CNTP_Transfer_pool_useTicketManager.ts

--- src/util/CNTP_Transfer_pool_useTicketManager.ts
+++ src/util/CNTP_Transfer_pool_useTicketManager.ts
@@ -9,7 +9,7 @@ const rpcUrl = 'https://rpc.conet.network'
 const CNTP_TicketManager_addr = '0x3Da23685785F721B87971ffF5D0b1A892CC1cd71'
 const transferTimeout = 1000 * 180			//	3 mins
 const checkGasPrice = 1550000
-const longestWaitingTime = 1000 * 60 * 15	//	15 mins
+const longestWaitingTime = 1000 * 60 * 5	//	15 mins
 
 
 interface paymentItem {
