773699d980b0e25876dbef90909ca77dfedb20ce
CoNET Project <info@CoNETTech.ca>
2024-10-14 12:09:47 -0700

    test

--- src/endpoint/serverApiV4.ts
+++ src/endpoint/serverApiV4.ts
@@ -52,13 +52,15 @@ const filePath = '/home/peter/.data/v2/'
 const get_epoch_total = async () => {
 	const block = currentEpoch - 1
 	const filename1 = `${filePath}${block}.total`
-	const data = await readFile(filename1, 'utf8')
+	
 	try {
+		const data = await readFile(filename1, 'utf8')
 		const ratedata = JSON.parse(data)
 		eposh_total.set(block, ratedata)
 		eposh_total.delete(block - 2)
 	} catch (ex) {
-		logger(Colors.red(`get_epoch_total JSON.parse(data) Error!\n ${data}`))
+		eposh_total.set(block, {totalMiners: 0, minerRate: 0})
+		logger(Colors.red(`get_epoch_total JSON.parse(data) Error!`))
 	}
 }
 
