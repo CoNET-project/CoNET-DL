5b5f1292bac9ac73083297af4cc30841e552995c
CoNET Project <info@CoNETTech.ca>
2024-10-14 15:10:37 -0700

    test

--- src/util/layerMinusClientV2.ts
+++ src/util/layerMinusClientV2.ts
@@ -196,7 +196,7 @@ const connectToGossipNode = async (node: nodeInfo ) => {
 			const users = data.userWallets||[]
 			node.lastEposh =  data.epoch
 			//logger(Colors.grey(`startGossip got ${node.ip_addr} wallets miners ${data.nodeWallets.length} users ${data.userWallets.length}`))
-			postLocalhost('/miningData', {wallets, users, ipaddress: node.ip_addr})
+			postLocalhost('/api/miningData', {wallets, users, ipaddress: node.ip_addr})
 		} catch (ex) {
 			logger(Colors.blue(`${node.ip_addr} => \n${_data}`))
 			logger(Colors.red(`connectToGossipNode ${node.ip_addr} JSON.parse(_data) Error!`))
