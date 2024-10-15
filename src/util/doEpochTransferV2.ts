6b6ac0d1585812b1e366f4ec641c187a21610631
CoNET Project <info@CoNETTech.ca>
2024-10-14 19:32:16 -0700

    Update doEpochTransferV2.ts

--- src/util/doEpochTransferV2.ts
+++ src/util/doEpochTransferV2.ts
@@ -71,7 +71,7 @@ const startListeningCONET_Holesky_EPOCH_v2 = async () => {
 
 	provider.on('block', async (_block: number) => {
 		if (_block === EPOCH + 1) {
-			stratFreeMinerTransfer(_block - 4)
+			stratFreeMinerTransfer(_block - 2)
 			EPOCH ++
 		}
 	})
