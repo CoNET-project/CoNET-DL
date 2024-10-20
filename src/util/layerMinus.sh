5f37a34836f4f14ba0148326e513e90b6df292a4
CoNET Project <info@CoNETTech.ca>
2024-10-19 20:17:45 -0700

    www

--- src/util/layerMinus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/usr/bin/node dist/util/layerMinusClientV2.js S=0 n=50 &
-/usr/bin/node dist/util/layerMinusClientV2.js S=50 n=100 &
-/usr/bin/node dist/util/layerMinusClientV2.js S=100 n=131 &
