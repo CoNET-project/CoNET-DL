9b4f7a84febe9f54017311c52c9e27f3009c8cca
CoNET Project <info@CoNETTech.ca>
2025-01-28 06:19:22 -0800

    conet-depin

--- src/endpoint/CoNET_DePIN-MainChain.ts
+++ src/endpoint/CoNET_DePIN-MainChain.ts
@@ -5,11 +5,12 @@ import CoNETDePINMainnetABI from './CoNETDePINMainnet.json'
 import CONETDePIN_Airdrop from './CONETDePINBridge.json'
 import Colors from 'colors/safe'
 import {inspect} from 'node:util'
+import {mapLimit} from 'async'
 
 const CoNETMainChainRPC = 'http://38.102.126.53:8000'
 const CoNETHoleskyRPC = 'https://rpc.conet.network'
 const CoNETDePINMainchainSC = '0xc4C9927516db9BBe42DC0b003A7AB0946AC649C1'
-const CoNETDePINHoleskySCAddress = '0xF837a46A0dD04dc27c46260d1C0cfC00628Ec517'.toLowerCase()
+const CoNETDePINHoleskySCAddress = '0xa0822b9fe34f81dd926ff1c182cb17baf50004f7'.toLowerCase()
 const CoNETDePINMainchainBridgeAddress = '0x242D503a29EaEF650615946A8C5eB2CD6c0164d6'
 
 const endPointHolesky = new JsonRpcProvider(CoNETHoleskyRPC)
@@ -20,22 +21,49 @@ const CoNETDePINHoleskySC = new Contract(CoNETDePINHoleskySCAddress, CoNETDePINH
 const CoNETDePINMainnetSC = new Contract(CoNETDePINMainchainSC, CoNETDePINMainnetABI, CoNETDePINMainnetAdmin)
 const CoNETDePINMainchainBridgeSC = new Contract(CoNETDePINMainchainBridgeAddress, CONETDePIN_Airdrop, CoNETDePINMainnetAdmin)
 
+interface transferData {
+	toAddress: string
+	value: BigInt
+	hash: string
+}
+const transferPool: transferData[] = []
+let transferProcess = false
+
+const _transfer = async () => {
+	if (transferProcess) {
+		return
+	}
+	transferProcess = true
+	const data = transferPool.shift()
+	if (!data) {
+		transferProcess = false
+		return
+	}
+	logger(inspect(data, false, 3, true))
+	try {
+		const tx = await CoNETDePINMainchainBridgeSC.airDrop(data.hash, data.toAddress, data.value)
+		const kk = await tx.wait()
+		logger(inspect(kk))
+	} catch (ex: any) {
+		logger(Colors.red(`CoNETDePINMainchainBridgeSC.airDrop Error! ${ex.message}`))
+	}
+	transferProcess = false
+	_transfer()
+}
+
+
 const checkTransfer = async (tR: TransactionReceipt) => {
 	for (let log of tR.logs) {
 		const LogDescription = CoNETDePINHoleskySC.interface.parseLog(log)
 		if (LogDescription?.name === 'bridgeTo') {
 			const toAddress  = LogDescription.args[0]
-			const value = LogDescription.args[1]
+			const value: BigInt = LogDescription.args[1]
 			const hash = tR.hash
-			logger(Colors.magenta(`Bridge hash[${hash}] toAddress[${toAddress}] value[${value}]`))
-			try {
-				const tx = await CoNETDePINMainchainBridgeSC.airDrop(hash, toAddress, value)
-				
-				logger(inspect(tx))
-			} catch (ex: any) {
-				logger(Colors.red(`CoNETDePINMainchainBridgeSC.airDrop Error!`), ex.message)
-			}
+			transferPool.push ({toAddress, value, hash})
+			_transfer()
 			
+		} else {
+			logger(LogDescription?.name)
 		}
 	}
 	
@@ -50,22 +78,22 @@ const holeskyBlockListenning = async (block: number) => {
 	if (!blockTs?.transactions) {
         return logger(Colors.gray(`holeskyBlockListenning ${block} has none`))
     }
+	logger(Colors.gray(`holeskyBlockListenning START ${block}`))
 
 	for (let tx of blockTs.transactions) {
 
 		const event = await getTx(tx)
 		
 		if ( event?.to?.toLowerCase() === CoNETDePINHoleskySCAddress) {
-			return checkTransfer(event)
-		}
-		logger(Colors.gray(`holeskyBlockListenning ${block} has none of CoNETDePINHoleskySCAddress!`))
+			checkTransfer(event)
+		} 
+		
 	}
 }
 
 const getTx = async (tx: string) => {
 	return await endPointHolesky.getTransactionReceipt(tx)
 }
-
 
 
 const daemondStart = () => {
