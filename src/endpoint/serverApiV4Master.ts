38a2a7de1733d2143bfb12e803f534ca5a2a82ae
CoNET Project <info@CoNETTech.ca>
2025-02-08 10:41:17 -0800

    Update serverApiV4Master.ts

--- src/endpoint/serverApiV4Master.ts
+++ src/endpoint/serverApiV4Master.ts
@@ -238,7 +238,7 @@ const moveData = async () => {
 	const rateSC = new ethers.Contract(rateAddr, rateABI, provideCONET)
 	const rate = parseFloat(ethers.formatEther(await rateSC.miningRate()))
 
-	const block = currentEpoch - 1
+	const block = currentEpoch
 	
 	let _wallets_: Map<string, true> = new Map()
 	let _users_: Map<string, true> = new Map()
