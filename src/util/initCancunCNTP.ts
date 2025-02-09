8913435979e0be8c261d3cca96cac7a7e5cae141
CoNET Project <info@CoNETTech.ca>
2025-02-09 07:58:34 -0800

    GuardianInit

--- src/util/initCancunCNTP.ts
+++ src/util/initCancunCNTP.ts
@@ -9,7 +9,9 @@ import CONET_Point_ABI from './cCNTP.json'
 import rateABI from '../endpoint/conet-rate.json'
 import CoNETDePINMiningABI from './CoNETDePINMiningABI.json'
 import CoNET_CancunRefferABI from './CONETDePINReferralABI.json'
-
+import {abi as CONET_Referral_ABI} from '../util/conet-referral.json'
+import GuardianPlan_new_ABI from './GuardianNodesV2.json'
+import GuardianInitABI from './GuardianInitABI.json'
 
 const CONET_HoleskyRPC = 'https://rpc.conet.network'
 const CoNET_CancunRPC = 'http://207.90.195.48:8001'
@@ -20,12 +22,17 @@ const cCNTP_holeskyAddr = '0xa4b389994A591735332A67f3561D60ce96409347'
 const CoNETDePINMiningContract = '0x3B91CF65A50FeC75b9BB69Ded04c12b524e70c29'
 const cntpHolesky = new ethers.Contract(cCNTP_holeskyAddr, CONET_Point_ABI, provode_Holesky)
 
-import {abi as CONET_Referral_ABI} from '../util/conet-referral.json'
+const GuardianInitAddr = '0xAA32dE55fcf35fa0A6F5ece21539E04f2ECee21c'
+
 
 const RefferV4_HoleskyAddr = '0x1b104BCBa6870D518bC57B5AF97904fBD1030681'
 const RefferV4_CancunAddr = '0xbd67716ab31fc9691482a839117004497761D0b9'
+const Guardian_Holesky = '0x35c6f84C5337e110C9190A5efbaC8B850E960384'
+
 const referralsV3_Holesky_Contract = new ethers.Contract(RefferV4_HoleskyAddr, CONET_Referral_ABI, provode_Holesky)
 const referralsV3_Cancun_Contract = new ethers.Contract(RefferV4_CancunAddr, CoNET_CancunRefferABI, provode_Cancun)
+
+const GuardianP_HoleskySC = new ethers.Contract(Guardian_Holesky, GuardianPlan_new_ABI, provode_Holesky)
 const adminList=[
 	"0x6add8012d4DDb7dA736Ab713FdA13ef3827a05bf",
 	"0x068759bCfd929fb17258aF372c30eE6CD277B872",
@@ -37,6 +44,8 @@ const adminList=[
 	"0x299DF9eF2C011aaEc3587e296A78E8e75838B2F7",
 	"0x830D4476A1FdF98bd6515353073527316DC315f1"
 ]
+const GuardianInitWallet = new ethers.Wallet(masterSetup.guardianAmin[0], provode_Cancun)
+const initGuardianSC = new ethers.Contract(GuardianInitAddr, GuardianInitABI, GuardianInitWallet)
 
 const addAdmin = () => {
 	const adminWallet = new ethers.Wallet(masterSetup.guardianAmin[3], provode_Cancun)
@@ -157,7 +166,59 @@ export const refferInit = async (wallet: string, reffer: string) => {
 	ReffeProcess.push ({wallet, reffer})
 }
 
+interface GroudinerNFTData {
+	wallet: string
+	nft1: BigInt
+	nft2: BigInt
+}
+
+const initGroudinerNFTPool: GroudinerNFTData[] = []
+
+const processInitGroudinerNFT = async () => {
+	if (!initGroudinerNFTPool.length) {
+		return
+	}
+	const _data = initGroudinerNFTPool.shift()
+	if (!_data) {
+		return
+	}
+
+	try {
+		const tx = await initGuardianSC.initGuardian(_data.wallet, _data.nft1, _data.nft2)
+		await tx.walt()
+		logger(Colors.gray(`processInitGroudinerNFT ${_data.wallet} Success ! hash = ${tx.hash}`))
+
+	} catch (ex: any) {
+		logger(Colors.grey(`processInitGroudinerNFT Error, ${ex.message}`))
+	}
+	processInitGroudinerNFT()
+}
+
+
+const checkGroudinerNFT = async (wallet: string) => {
+
+	let nft1: BigInt, nft2: BigInt
+	let isInit: boolean
+	[nft1, nft2, isInit] = await Promise.all ([
+		GuardianP_HoleskySC.balanceOf(wallet, 1),
+		GuardianP_HoleskySC.balanceOf(wallet, 2),
+		initGuardianSC.isInit(wallet)
+	])
+
+	if (nft1 == BigInt(0) && nft2 == BigInt(0) && !isInit) {
+		return
+	}
+
+	initGroudinerNFTPool.push({
+		wallet, nft1, nft2
+	})
+
+	processInitGroudinerNFT()
+	
+}
+
 export const initCNTP = async (wallet: string) => {
+
 	if (wallet === ethers.ZeroAddress ) {
 		return
 	}
@@ -173,6 +234,8 @@ export const initCNTP = async (wallet: string) => {
 	walletProcess.push({
 		wallet, value
 	})
+
+	checkGroudinerNFT(wallet)
 }
 
 
@@ -180,3 +243,6 @@ export const startProcess = async () => {
 	startProcess_CNTP()
 	startProcess_Reff()
 }
+
+
+checkGroudinerNFT('0x69237C9B639577d5F8A2A8970B76A92fCbeE3C34')
