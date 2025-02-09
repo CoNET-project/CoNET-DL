7d88909f876023f06305a272b24b9e1d5538adb1
CoNET Project <info@CoNETTech.ca>
2025-02-08 21:14:12 -0800

    Update util.ts

--- src/util/util.ts
+++ src/util/util.ts
@@ -33,7 +33,7 @@ import {abi as erc20TokenABI} from './erc20.json'
 import {Readable} from 'node:stream'
 
 export const conet_Holesky_rpc1 = 'https://rpc.conet.network'
-export const conet_cancun_rpc = `http://207.90.195.48:8001`
+export const conet_cancun_rpc = `http://207.90.195.48:8002`
 const bscMainchainRPC = 'https://bsc-dataseed.bnbchain.org/'
 const balstMainchainRPC = 'https://rpc.ankr.com/blast'
 
