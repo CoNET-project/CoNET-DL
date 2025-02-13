f734fe76ded0694b1aab3f5e758de47f40c20f0d
CoNET Project <info@CoNETTech.ca>
2025-02-13 12:21:45 -0800

    Update CoNET_DePIN-CancunBridgeScanBloack.ts

--- src/endpoint/CoNET_DePIN-CancunBridgeScanBloack.ts
+++ src/endpoint/CoNET_DePIN-CancunBridgeScanBloack.ts
@@ -151,10 +151,9 @@ if (start_block > stop_block) {
 }
 
 
-// mapLimit(blockArray, 1, async (n, next) => {
-// 	await CancunBlockListenning(n)
-// }, err => {
-// 	logger(Colors.red(`Scan end!`))
-// })
+mapLimit(blockArray, 1, async (n, next) => {
+	await CancunBlockListenning(n)
+}, err => {
+	logger(Colors.red(`Scan end!`))
+})
 
-CancunBlockListenning(91194)
