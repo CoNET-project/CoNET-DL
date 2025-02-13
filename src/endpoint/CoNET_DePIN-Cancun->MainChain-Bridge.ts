8573b5e8e59fcdbc82bea15a420d5dd354c60488
CoNET Project <info@CoNETTech.ca>
2025-02-13 15:34:58 -0800

    clean data

--- src/endpoint/CoNET_DePIN-Cancun->MainChain-Bridge.ts
+++ src/endpoint/CoNET_DePIN-Cancun->MainChain-Bridge.ts
@@ -1,29 +1,23 @@
 import ethers, {JsonRpcProvider, Contract, Wallet, TransactionResponse, TransactionReceipt, formatEther, BigNumberish} from 'ethers'
 import {logger, masterSetup} from '../util/util'
-import CONETDePIN_Airdrop from './CNTPairdrop.json'
 import Colors from 'colors/safe'
 import {inspect} from 'node:util'
-import Cancun_CNTP_airdorpABI from '../util/Cancun_CNTP_airdorpABI.json'
-import CoNETDePIN_mainnet_airdropABI from './CoNET_DePIN_Mainnet_airdrop_SC.json'
 import CONET_Point_ABI from '../util/cCNTP.json'
 
+import CoNET_DePIN_ABI from './CoNET_DePIN.json'
+
+const CoNET_DePIN_addr = '0xc4D5cc27026F52dc357cccD293549076a6b7757D'
 
 const CoNETMainChainRPC = 'https://mainnet-rpc.conet.network'
 const CoNET_CancunRPC = 'https://cancun-rpc.conet.network'
-const CoNETDePINMainchainSC = '0xc4C9927516db9BBe42DC0b003A7AB0946AC649C1'
-const CoNETDePINMainchainBridgeAddress = '0xf093e5534dBd1E1fB52E29E5432C503876E658C2'
 
-const CoNETDePINCancunSCAddress = '0x8A8898960B45AEa683b36EB214422740cb19fD06'.toLowerCase()
 
 const CNTPCancun_addr = '0x6C7C575010F86A311673432319299F3D68e4b522'.toLowerCase()
 
 const endPointCancun = new JsonRpcProvider(CoNET_CancunRPC)
 const endPointCoNETMainnet = new JsonRpcProvider(CoNETMainChainRPC)
 
-const CoNETDePINMainnetAdmin = new Wallet(masterSetup.conetDePINAdmin[0], endPointCoNETMainnet)
-const CoNETDePIN_CNTP_Bridge_Event_readonly = new Contract(CoNETDePINCancunSCAddress, Cancun_CNTP_airdorpABI, endPointCancun)
 
-const CoNETDePINMainchainBridgeSC = new Contract(CoNETDePINMainchainBridgeAddress, CONETDePIN_Airdrop, CoNETDePINMainnetAdmin)
 const CNTP_Sc_readonly = new Contract(CNTPCancun_addr, CONET_Point_ABI, endPointCancun)
 
 interface transferData {
@@ -37,7 +31,7 @@ const transferPool: transferData[] = []
 const ecPool: Contract[] = []
 for (let _wa of masterSetup.newFaucetAdmin) {
 	const wa = new Wallet(_wa, endPointCoNETMainnet)
-	const sc = new Contract(CoNETDePINMainchainBridgeAddress, CoNETDePIN_mainnet_airdropABI, wa)
+	const sc = new Contract(CoNET_DePIN_addr, CoNET_DePIN_ABI, wa)
 	ecPool.push(sc)
 }
 
@@ -55,7 +49,7 @@ const _transfer = async () => {
 	}
 	
 	try {
-		const tx = await SC.airdrop(data.toAddress, data.value, data.hash)
+		const tx = await SC.airdropFromCancun(data.toAddress, data.value, data.hash)
 		const kk = await tx.wait()
 		logger(Colors.blue(`airDrop ${data.toAddress} ${formatEther(data.value)} success! hash = ${tx.hash}`))
 	} catch (ex: any) {
@@ -66,26 +60,6 @@ const _transfer = async () => {
 }
 
 
-const checkTransfer = async (tR: TransactionReceipt) => {
-
-	for (let log of tR.logs) {
-		const LogDescription = CoNETDePIN_CNTP_Bridge_Event_readonly.interface.parseLog(log)
-		logger(`${LogDescription?.name}`)
-		if (LogDescription?.name === 'bridgeTo') {
-			const toAddress  = LogDescription.args[0]
-			const value: BigNumberish = LogDescription.args[1]
-			const hash = tR.hash
-			const obj = {toAddress, value, hash}
-			transferPool.push (obj)
-			logger(inspect(obj, false, 3, true))
-			_transfer()
-			
-		} else {
-			logger(LogDescription?.name)
-		}
-	}
-	
-}
 
 const checkCNTPTransfer = async (tR: TransactionReceipt) => {
 	
