8f333148565957e7ee79917a178be88d64437513
CoNET Project <info@CoNETTech.ca>
2025-02-22 04:16:38 -0800

    Update CoNET_DePIN-MainChain_ETH-bridge.ts

--- src/endpoint/CoNET_DePIN-MainChain_ETH-bridge.ts
+++ src/endpoint/CoNET_DePIN-MainChain_ETH-bridge.ts
@@ -133,7 +133,7 @@ const daemondStart = async () => {
 
 		logger(inspect(feeObj, false, 3, true))
 		const _gasPrice = parseInt(feeObj.gasPrice.toString())
-		if ((Math.abs( _gasPrice - gasPrice )* 100 / gasPrice) > 4) {
+		if ((Math.abs( _gasPrice - gasPrice ) * 100) / gasPrice > 4) {
 			gasPrice = _gasPrice
 			logger(Colors.magenta(`Gas price over _gasPrice = ${_gasPrice} gasPrice = ${gasPrice} Math.abs( _gasPrice - gasPrice )* 100 / gasPrice) = ${(Math.abs( _gasPrice - gasPrice) * 100 )/ gasPrice} block number = ${block}`))
 			voteGasprice(_gasPrice, block)
