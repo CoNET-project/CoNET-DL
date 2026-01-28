7a6df87da352326aaeaefa37d3a080a41ff8799f
CoNET Project <info@CoNETTech.ca>
2026-01-28 14:23:46 -0800

    update

--- src/util/layerMinusClientV2.ts
+++ src/util/layerMinusClientV2.ts
@@ -223,7 +223,7 @@ const postLocalhost = (path: string, obj: any)=> new Promise(async resolve =>{
 			return resolve(false)
 		}
 		resolve(true)
-		//return logger(Colors.grey(`postLocalhost http://localhost/${path} Success!!!`),inspect(obj, false, 3, true))
+		return logger(Colors.grey(`postLocalhost http://localhost/${path} Success!!!`),inspect(obj, false, 3, true))
 
 	})
 
